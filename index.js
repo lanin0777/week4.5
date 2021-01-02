d6c48f4da48d317da5baa982c5ad34e7a3c53cd7
lanin0777 <73796876+lanin0777@users.noreply.github.com>
2021-01-02 22:47:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 // require('http').Server{}.listen();
-import {'Server'} from 'http';
-const s = Server();
-s.addListener('request', (req, res) => {
-    res.end('OK\n');
+//import {'Server'} from 'http';
+//const s = Server();
+//s.addListener('request', (req, res) => {
+//    res.end('OK\n');
+//});
+//s.listen(4321);
+const CORS = {
+  'Access-Control-Allow-Origin': 'https://kodaktor.ru',
+  'Access-Control-Allow-Methods': 'GET,PUT,DELETE',
+  'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
+};
+const s = require('http').Server((req, res) => {
+    if (req.url === '/ru') {
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8', ...CORS });
+        res.write('<h1><i>Да</i></h1>\n'); 
+    } else if (req.url === '/doc/new/vasya-new.htm') {
+        res.write('<h1><i>Vasya Web Page</i></h1>\n');
+    } else if (req.url === '/download') {
+        res.writeHead(200, { 
+         'Content-Disposition': 'attachment; filename="file.txt"'
+        });
+        res.write('Файл\n');        
+    } else {
+        res.write('OK\n');
+    }
+    res.end();
 });
-s.listen(4321);
+s.listen(4321); 
